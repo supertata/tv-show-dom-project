9745d0a76f1f5b3e885808f9c8da522ff183c860
supertata <laetitiachira@gmail.com>
2020-06-22 20:52:03 +0100

    added episode img

--- script.js
+++ script.js
@@ -20,19 +20,24 @@ function makePageForEpisodes(episodeList) {
   for (i=0; i < episodeList.length; i++) {   //pour chaque episode
     let episode = document.createElement("div"); //cree un element div
     document.querySelector("#episodeContainer").appendChild(episode);  //append the div to the wrapper div
-    // episode.textContent = `${episode[i].name}` //
     episode.textContent = `${episodeList[i].name} - S0${episodeList[i].season}0${episodeList[i].number}
     ${episodeList[i].summary}`;
+    episode.id = `episode${1 + i}`; //set the id of the episode divs from 0 to .length
+    episode.appendChild(document.createElement("img")).src = episodeList[i].image.medium; //create an img and dynamically append it to new div
 }
 }
 
 
 window.onload = setup;
 
-// episode.id = `episode${1 + i}`; //set the id of the episode divs from 0 to .length
-    // let episodeImage = document.createElement("img"); //cree un element img
-    // document.getElementById("#episodeContainer").appendChild(episodeImage); //append img to episode
-    // episodeImage.src = episodeList[i].image.medium; //set the source of the img tag 
+// function addEpisodeImage(episodeList) {
+//   for (i=0; i < episodeList.length; i++) {   //pour chaque episode
+//   let episodeImage = document.createElement("img"); //cree un element img
+//   document.getElementById("#episodeContainer").appendChild(episodeImage); //append img to episode
+//   episodeImage.src = episodeList[i].image.medium; //set the source of the img tag 
+// }
+// }
+
 
 
 
