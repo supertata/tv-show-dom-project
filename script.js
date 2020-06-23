33d87b87c95257263708b3e7688b427f3a7cf603
supertata <laetitiachira@gmail.com>
2020-06-23 16:42:25 +0100

    refactored code for makeEpisodePage

--- script.js
+++ script.js
@@ -9,34 +9,46 @@
 //   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 // }
 
-
 function setup() {
   const allEpisodes = getAllEpisodes();
+  // removePtag (allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  
-  for (i=0; i < episodeList.length; i++) {   //pour chaque episode
+
+  let parent = document.querySelector("#episodeContainer");
+  for (let i = 0; i < episodeList.length; i++) {
+    //pour chaque episode
     let episode = document.createElement("div"); //cree un element div
-    document.querySelector("#episodeContainer").appendChild(episode);  //append the div to the wrapper div
-    episode.textContent = `${episodeList[i].name} - S0${episodeList[i].season}0${episodeList[i].number}
-    ${episodeList[i].summary}`;
-    episode.id = `episode${1 + i}`; //set the id of the episode divs from 0 to .length
-    episode.appendChild(document.createElement("img")).src = episodeList[i].image.medium; //create an img and dynamically append it to new div
-}
+    let title = document.createElement("p");
+    let image = document.createElement("img");
+    let summary = document.createElement("p");
+
+    image.setAttribute('src', episodeList[i].image.medium);
+
+    parent.appendChild(episode);
+    episode.appendChild(title);
+    episode.appendChild(image);
+    episode.appendChild(summary);
+
+    episode.style.textAlign = "center";
+    title.style.margin = "0 30px 40px 30px";
+    title.style.fontSize = "x-large";
+    // image.style.width = "100%";
+    // image.style.height = auto;
+
+    summary.style.margin = "30px 30px 20px 30px";
+    summary.style.textAlign = "justify";
+
+    title.textContent = `${episodeList[i].name} - S0${episodeList[i].season}E0${episodeList[i].number}`;
+    summary.textContent = `${episodeList[i].summary}`;
+    
+  }
 }
 
 
 window.onload = setup;
-
-// function addEpisodeImage(episodeList) {
-//   for (i=0; i < episodeList.length; i++) {   //pour chaque episode
-//   let episodeImage = document.createElement("img"); //cree un element img
-//   document.getElementById("#episodeContainer").appendChild(episodeImage); //append img to episode
-//   episodeImage.src = episodeList[i].image.medium; //set the source of the img tag 
-// }
-// }
 
 
 
